f43cf971dccfc62104af787062bf3656bdb6e789
Jeremias Aitor <oyhamburo.jeremias@gmail.com>
2022-08-23 19:12:37 -0300

    Modificando mock

--- src/componets/utils/mock.js
+++ src/componets/utils/mock.js
@@ -1,16 +1,81 @@
 const combos = [
     {   
-        name: 'Combo Infierno',
-        id: 1,
-        simple: 'Infierno Simple',
-        priceSimple: 800,
-        doble: 'Infierno Doble',
-        priceDoble: 900,
-        triple: 'Infierno Triple',
-        priceTriple: 1000,
+        Type: 'Combo Infierno',
         estado: 1,
-        descrip: 'Carne, chedar y bacon',
-        opciones: 5,
+        meat: [
+            {
+                name:'Infierno Simple',
+                price: 850,
+                descrip: 'Carne x1, chedar y bacon',
+                src: './hamburguesa.jpg'
+            },
+            {
+                name:'Infierno Doble',
+                price: 950,
+                descrip: 'Carne x2, chedar y bacon',
+                src: './hamburguesa.jpg'
+            },
+            {
+                name:'Infierno Triple',
+                price: 1050,
+                descrip: 'Carne x3, chedar y bacon',
+                src: './hamburguesa.jpg'
+            },
+            {
+                name:'Infierno Cuadruple',
+                price: 1150,
+                descrip: 'Carne x4, chedar y bacon',
+                src: './hamburguesa.jpg'
+            },
+            {
+                name:'Infierno Quintuple',
+                price: 1250,
+                descrip: 'Carne x5, chedar y bacon',
+                src: './hamburguesa.jpg'
+            },
+        ]
+    },
+    {   
+        Type: 'Combo Big Hell',
+        estado: 1,
+        meat: [
+            {
+                name:'Big Hell',
+                price: 950,
+                descrip: 'Carne x2, chedar y bacon',
+                src: './hamburguesa.jpg'
+            },
+            {
+                name:'Big Hell triple',
+                price: 1050,
+                descrip: 'Carne x3, chedar y bacon',
+                src: './hamburguesa.jpg'
+            },
+        ]
+    },
+    {   
+        Type: 'Combo paradise',
+        estado: 1,
+        meat: [
+            {
+                name:'Paradise',
+                price: 950,
+                descrip: 'Carne x1, chedar y bacon',
+                src: './hamburguesa.jpg'
+            },
+            {
+                name:'Paradise Doble',
+                price: 950,
+                descrip: 'Carne x2, chedar y bacon',
+                src: './hamburguesa.jpg'
+            },
+            {
+                name:'Big Hell triple',
+                price: 1050,
+                descrip: 'Carne x3, chedar y bacon',
+                src: './hamburguesa.jpg'
+            },
+        ]
     },
 ]
 export default combos
